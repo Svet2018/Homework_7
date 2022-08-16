9c42bbede05a5d653c6dc12aa044333ca4b007a5
Svetlana <bulatova44@gmail.com>
2022-08-16 18:04:50 +0300

    The program works with index and number

--- Task_50/Program.cs
+++ Task_50/Program.cs
@@ -1,4 +1,7 @@
-﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента либо значение элемента в двумерном массиве, и возвращает значение либо индекс этого элемента или же указание, что такого элемента нет.
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента 
+//либо значение элемента в двумерном массиве, 
+//и возвращает значение либо индекс этого элемента 
+//или же указание, что такого элемента нет.
 //Например, задан массив:
 //1 4 7 2
 //5 9 2 3
@@ -7,3 +10,82 @@
 //4 -> такой элемент есть и его индекс 0, 1 (идеально было б найти все вхождения этого элемента)
 //2, 3 -> такой элемент есть и равен 4
 //5, 5 -> такой элемент отсутствует
+
+
+
+void GenerateArray(int [,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+           array[i,j] = new Random().Next(1,101);
+        }
+        
+    } 
+}
+
+
+void PrintArray(int [,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i,j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int row = new Random().Next(9,10);
+int col = new Random().Next(9,10);
+
+int [,] newArray = new int [row, col];
+
+GenerateArray(newArray);
+PrintArray(newArray);
+
+System.Console.WriteLine($"Введите значение элемента (например: 19) или позицию элемента через запятую (например: 5,2)");
+string value = Console.ReadLine();
+//System.Console.WriteLine($"value - {value}");
+
+bool contains = value.Contains(',');
+
+string[] splitdata = value.Split(',');
+
+
+/*
+if (contains0 > row | contains1 > col)
+{
+    System.Console.WriteLine($"Такой элемент отсутствует");
+    System.Console.WriteLine();
+}
+*/
+if (contains == true)
+{  
+    
+    int contains0 = Convert.ToInt32(splitdata[0]);
+    int contains1 = Convert.ToInt32(splitdata[1]);
+    System.Console.WriteLine($"Вы ввели позицию элемента ({value}), что соответствует числу {newArray[contains1, contains0]}");
+}
+else
+{
+    int numLength = Convert.ToInt32(value);
+    
+    for (int i = 0; i < row; i++)
+    {
+        
+        for (int j = 0; j < col; j++)
+        {
+           
+            if (numLength == newArray[i,j])
+            {
+                System.Console.WriteLine($"Индекс введенного числа {value} в массиве равен ({i} , {j})");
+                
+            }
+            
+                    
+        }
+    }
+}
